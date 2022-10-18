f09e66ff025afd465c5ffc3920c1adb02c1cd3ae
YuNiling <1689272480@qq.com>
2022-10-18 20:39:02 +0800

    配置vuex

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Home</h1>
+  <h3 @click="increment">{{ count }}</h3>
 </template>
+
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+const store = useStore()
+const count = computed(() => store.state.count)
+const increment = () => {
+  store.commit('increment')
+}
+</script>
